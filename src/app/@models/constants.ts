b7f7b1c9343092f46f1588805a972442a4790e76
Luis Huertas <hu3rtas@outlook.com>
2022-11-10 11:12:06 -0500

    Initial commit

---
+++ src/app/@models/constants.ts
@@ -0,0 +1,61 @@
+export const ES_CALENDAR = {
+  firstDayOfWeek: 1,
+  dayNames: [
+    'Domingo',
+    'Lunes',
+    'Martes',
+    'Miercoles',
+    'Jueves',
+    'Viernes',
+    'Sabado',
+  ],
+  dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'],
+  dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'],
+  monthNames: [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre',
+  ],
+  monthNamesShort: [
+    'Ene',
+    'Feb',
+    'Mar',
+    'Abr',
+    'May',
+    'Jun',
+    'Jul',
+    'Ago',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dic',
+  ],
+  today: 'Hoy',
+  clear: 'Clear',
+  dateFormat: 'mm/dd/yy',
+  weekHeader: 'Wk',
+  dateType: 'string',
+  currentText: 'Fecha actual',
+  firstDay: 1,
+};
+
+export const MONTHS_RANGE = 3;
+
+export const LENGTH_TO_ALLOW_AUTO_COMPLETE = 4;
+
+export const JWT_TOKEN = 'idToken';
+
+export const REFRESH_TOKEN = 'refreshToken';
+
+export const USER = 'username';
+
+export const NO_TOKEN_MESSAGE = 'A token was not send';
